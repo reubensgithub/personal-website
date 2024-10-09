a3a23cff1f216635a42d3f3474bf1ab6a8e72c1b
Reuben Kurian <reubenkurian03@yahoo.co.uk>
2024-10-09 21:42:44 +0100

    final changes for the day.

--- src/data/user.js
+++ src/data/user.js
@@ -1,22 +1,18 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Reuben's Portfolio",
+		name: "Reuben Kurian",
+		email: "reubenkurian03@yahoo.co.uk",
 		logo: "../logo.png",
 	},
 
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		github: "https://github.com/reubensgithub",
+		linkedin: "https://linkedin.com/in/reuben-kurian-5a32ba302"
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Final year MSci Computer Science Student at University of Exeter",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
